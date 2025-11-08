22b6b3b3a9d7efddfa02105f017082eb9e9b663f
Mrsikarwar27 <nabhsikarwar@gmail.com>
2025-11-08 10:43:31 +0530

    Refactor code by commenting out unused jsUser object and related methods for clarity

--- js-question/01_objects.js
+++ js-question/01_objects.js
@@ -2,39 +2,73 @@
 
 // object literal syntax
 
-const  mySym = Symbol("key1")
-const jsUser = 
-    {
+// const  mySym = Symbol("key1")
+// const jsUser = 
+//     {
     
-     "name " : "vijay", //FIXME:key string ko access krne k liye bracket notation use krna padta hai
-     age: 30,
-     isAdmin: true,
-     courses: ["html", "css", "js"],
-        address: "gwalior",
-        email:"vijay@gmail.com",
-        [mySym]:  "mykey1",
+//      "name " : "vijay", //FIXME:key string ko access krne k liye bracket notation use krna padta hai
+//      age: 30,
+//      isAdmin: true,
+//      courses: ["html", "css", "js"],
+//         address: "gwalior",
+//         email:"vijay@gmail.com",
+//         [mySym]:  "mykey1",
    
       
 
-    }
-  //  console.log(jsUser["name "]); //FIXME: [] use kiaa h access krne k liye
-    //console.log(jsUser.name);// ue simple dot notation se access kr skte h
-   // console.log(jsUser[mySym]);
-jsUser.email= "nabhsikarwar@gmail.com"
-    //console.log(jsUser);
-   // Object.freeze(jsUser) // object ko freeze kr diya h ab hum isme koi bhi change nhi kr skte h
-    jsUser.email= "nabhtomar@gmail.com";
-   // console.log(jsUser);
+//     }
+//   //  console.log(jsUser["name "]); //FIXME: [] use kiaa h access krne k liye
+//     //console.log(jsUser.name);// ue simple dot notation se access kr skte h
+//    // console.log(jsUser[mySym]);
+// jsUser.email= "nabhsikarwar@gmail.com"
+//     //console.log(jsUser);
+//    // Object.freeze(jsUser) // object ko freeze kr diya h ab hum isme koi bhi change nhi kr skte h
+//     jsUser.email= "nabhtomar@gmail.com";
+//    // console.log(jsUser);
     
-    jsUser.greeting = function (){
-        console.log("hello  JS user");
-    }
+//     jsUser.greeting = function (){
+//         console.log("hello  JS user");
+//     }
    
-    jsUser.greetingTwo = function (){
-        console.log(`hello  JS user, ${this["name "]}`);
+//     jsUser.greetingTwo = function (){
+//         console.log(`hello  JS user, ${this["name "]}`);
         
-    }
+//     }
 
-    console.log(jsUser.greeting());
+//     console.log(jsUser.greeting());
     
-   console.log(jsUser.greetingTwo());
+//    console.log(jsUser.greetingTwo());
+
+
+// TODO: Destructuring 
+
+const courses ={
+    name : "vijay",
+    courses : {
+
+        html: {
+            tag: "<html>",
+
+            duration: {
+                time :"15 days",
+        
+
+    },
+    addreess :{
+     city : "gwalior",
+        state : "mp"
+
+    }
+    
+}
+}
+}
+// console.log(courses.courses.js);
+// let  { courses: {js: jsCourse}} = courses;
+// console.log(jsCourse); 
+
+console.log(courses.courses.html.duration.time);
+// FIXME: {this is cool how it is Destructuring we can simply by the time} 
+let {courses : {html: {duration:{time}}}} =courses;
+
+console.log(time);
